b088a35ae3a7881801ae718769ece28def14157a
HuascarFedor <huascar.fedor@gmail.com>
2024-11-18 16:18:35 -0400

    added partial database

--- lib/models/nota.dart
+++ lib/models/nota.dart
@@ -1,4 +1,20 @@
 class Nota {
+  final int? id;
   final String content;
-  Nota({this.content = ''});
+
+  Nota({this.id, this.content = ''});
+
+  factory Nota.fromMap(Map<String, dynamic> map) {
+    return Nota(
+      id: map['id'],
+      content: map['content'],
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      if (id != null) 'id': id,
+      'content': content,
+    };
+  }
 }
---
+++ lib/repositories/todo_repositorio.dart
@@ -0,0 +1,22 @@
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+
+class TodoRepository {
+  Database? _database;
+
+  Future<Database> get database async {
+    if (_database != null) return _database;
+    _database = await _initDB();
+    return _database;
+  }
+
+  Future<Database> _initDB() async {
+    final dbPath = await getDatabasesPath();
+    final path = join(dbPath, 'nota.db');
+    return await openDatabase(
+      path,
+      version: 1,
+      onCreate: _createDB,
+    );
+  }
+}
