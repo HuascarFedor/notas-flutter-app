bd77279706abe93c8682f353941cde0e8e10aaf9
HuascarFedor <huascar.fedor@gmail.com>
2024-11-22 15:24:56 -0400

    added todo_repository

--- lib/repositories/todo_repository.dart
+++ lib/repositories/todo_repository.dart
@@ -1,3 +1,4 @@
+import 'package:notas_flutter/models/nota.dart';
 import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
 
@@ -17,6 +18,32 @@ class TodoRepository {
       path,
       version: 1,
       onCreate: _createDB,
+    );
+  }
+
+  Future _createDB(Database db, int version) async {
+    await db.execute(
+        'CREATE TABLE Notas(id INTEGER PRIMARY KEY AUTOINCREMENT, content TEXT)');
+  }
+
+  Future<List<Nota>> getAllNotas() async {
+    final db = await database;
+    final List<Map<String, dynamic>> notasMap = await db.query('Notas');
+    return notasMap.map((notaMap) => Nota.fromMap(notaMap)).toList();
+  }
+
+  Future<int> insertNota(Nota nota) async {
+    final db = await database;
+    int id = await db.insert('Notas', nota.toMap());
+    return id;
+  }
+
+  Future<void> deleteNota(Nota nota) async {
+    final db = await database;
+    await db.delete(
+      'Notas',
+      where: 'id = ?',
+      whereArgs: [nota.id],
     );
   }
 }
