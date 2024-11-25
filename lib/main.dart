d424a0bdf3c2fceadfef2b1ca51ff47eec8b2938
HuascarFedor <huascar.fedor@gmail.com>
2024-11-25 14:54:33 -0400

    updated main

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:notas_flutter/controllers/nota_controller.dart';
 import 'package:notas_flutter/providers/nota_notifier.dart';
+import 'package:notas_flutter/repositories/todo_repository.dart';
+import 'package:notas_flutter/services/nota_service.dart';
 import 'package:provider/provider.dart';
 import './views/nota_screen.dart';
 
@@ -14,7 +16,8 @@ class NotasApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider(
-      create: (_) => NotaNotifier(NotaController()),
+      create: (_) =>
+          NotaNotifier(NotaController(NotaService(TodoRepository()))),
       child: const MaterialApp(
         debugShowCheckedModeBanner: false,
         home: NotaScreen(),
