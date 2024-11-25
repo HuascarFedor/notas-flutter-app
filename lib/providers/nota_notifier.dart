1c99553f9597b65b3209d2e3a92c1f5eb2451e85
HuascarFedor <huascar.fedor@gmail.com>
2024-11-25 14:52:13 -0400

    added nota_notifier

--- lib/providers/nota_notifier.dart
+++ lib/providers/nota_notifier.dart
@@ -4,18 +4,50 @@ import 'package:notas_flutter/models/nota.dart';
 
 class NotaNotifier extends ChangeNotifier {
   final NotaController _notaController;
+  bool _isLoading = false;
+  String? _errorMessage;
 
-  NotaNotifier(this._notaController);
+  NotaNotifier(this._notaController) {
+    _loadNotas();
+  }
 
+  bool get isLoading => _isLoading;
+  String? get errorMessage => _errorMessage;
   List<Nota> get notas => _notaController.notas;
+  int get notasCount => notas.length;
 
-  void addNota(String content) {
-    _notaController.addNota(content);
+  Future<void> _loadNotas() async {
+    try {
+      _isLoading = true;
+      notifyListeners();
+      await _notaController.loadNotas();
+    } catch (_) {
+      _errorMessage = "No se pudieron cargar los planes";
+    } finally {
+      _isLoading = false;
+      notifyListeners();
+    }
+  }
+
+  Future<void> addNota(String content) async {
+    try {
+      _errorMessage = null;
+      notifyListeners();
+      await _notaController.addNota(content);
+    } catch (_) {
+      _errorMessage = "Error al añadir la nota";
+    }
     notifyListeners();
   }
 
-  void deleteNota(Nota nota) {
-    _notaController.deleteNota(nota);
+  Future<void> deleteNota(Nota nota) async {
+    try {
+      _errorMessage = null;
+      notifyListeners();
+      await _notaController.deleteNota(nota);
+    } catch (_) {
+      _errorMessage = "Error al eliminar la nota";
+    }
     notifyListeners();
   }
 }
